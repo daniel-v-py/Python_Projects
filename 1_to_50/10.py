e12dbb1faa4455140575863c877de53c888b94e1
daniel-v-py <danielvirp@gmail.com>
2019-10-13 18:20:48 +0300

    Whitespace fixes to 10 after fucking around with ide

--- 1_to_50/10.py
+++ 1_to_50/10.py
@@ -1,6 +1,6 @@
 n = int(input("Please enter a number : "))
-print("Type of data : " ,type(n))
+print("Type of data : " , type(n))
 n1 = n
-n2 = int("%s%s" % (n,n))
-n3 = int("%s%s%s" % (n,n,n))
+n2 = int("%s%s" % (n , n))
+n3 = int("%s%s%s" % (n , n , n))
 print(n+n2+n3)
