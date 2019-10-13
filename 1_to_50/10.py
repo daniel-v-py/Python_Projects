5478c21b93134a142b86aead12b0dc15b80bc58b
daniel-v-py <danielvirp@gmail.com>
2019-10-13 17:53:10 +0300

    Fixes to 10, first commit was to test data type, and left it there just because!

--- 1_to_50/10.py
+++ 1_to_50/10.py
@@ -1,2 +1,6 @@
-number = int(input("Please enter a number : ")
-print("Type of data : ")
+n = int(input("Please enter a number : "))
+print("Type of data : " ,type(n))
+n1 = n
+n2 = int("%s%s" % (n,n))
+n3 = int("%s%s%s" % (n,n,n))
+print(n+n2+n3)
