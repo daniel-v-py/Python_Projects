833cfe2bfb87923f0d63fcbd66cff6b54c02075d
Daniel V <danielvirp@gmail.com>
2019-10-28 18:16:22 +0200

    Class 28/10/19

---
+++ Python_Class/import_sys.py
@@ -0,0 +1,9 @@
+import sys
+import os
+### command line arguments passed to the script
+var = sys.argv[0]
+
+print(var)
+if len(sys.argv) >= 2:
+    print(len(sys.argv))
+#### print length of amount of arguments
---
+++ Python_Class/import_time.py
@@ -0,0 +1,7 @@
+import time
+####
+time.sleep(5)
+### sleep on seconds
+print(time.time())
+#### time from 1/1/1970 in seconds
+print(f'{time.ctime()},{time.tzname}')
